4c3fe7a9d459ba1eb9522cee5ea950feb7a58a6e
Dhrumil <dhrumil.panchal@simformsolutions.com>
2025-03-13 10:46:40 +0530

    feat: added URL to manage different end points aand got basic idea for that

--- PR1_HTTP_SERVER/index.js
+++ PR1_HTTP_SERVER/index.js
@@ -1,12 +1,26 @@
 import http from "http";
-
+import { hostname } from "os";
+import { URL } from "url";
 const hotname = '127.0.0.1';
 const PORT =    3000;
 
 const server = http.createServer((req,res)=>{
+
+    const parsedUrl = new URL(req.url, `http://${hostname}:${PORT}`);
+    const path = parsedUrl.pathname;
    res.statusCode = 200;
-   res.setHeader('Content-type','text/plain');
-   res.end('Hello World');
+   res.setHeader('Content-type','application/json');
+    
+   switch(path)
+   {
+     case '/':
+        res.statusCode = 200;
+        res.end(JSON.stringify({message: 'Welcome'}));
+        break;
+     case '/books':
+        res.statusCode= 200;
+        res.end(JSON.stringify({message: 'List of books'}))
+   }
 });
 
 server.listen(PORT,hotname,()=>{
